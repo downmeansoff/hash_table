009e747d1de16097eada38d53b69f8473df27758
downmeansoff <116756888+downmeansoff@users.noreply.github.com>
2024-05-21 00:34:50 +0400

    added insert and insert_or_assign

--- include/hash_table.h
+++ include/hash_table.h
@@ -19,26 +19,82 @@ template <class Key, class Value>
 class HashTable {
 	std::vector<Pair<Key, Value>> _data;
 	size_t _size;
+	size_t _count_pairs;
+
 public:
-	HashTable(size_t size): _size(size) {
-		if (size == 0)  throw std::invalid_argument("invalid size");
+	HashTable(size_t size): _size(size), _count_pairs(0) {
+		if (size == 0)  throw std::invalid_argument("size = 0");
 		_data.resize(size);
 	}
 
 	HashTable(size_t size, const Value& min, const Value& max){
-		if (size == 0) throw std::invalid_argument("size = 0");//создаем хеш таблицу, заполненную рандомными значениями
-		_size = size;
+		if (size == 0) throw std::invalid_argument("size = 0");
+		_size = size * 1.7;
+		_count_pairs = size;
 		_data.resize(size);
 		for (size_t i = 0; i < size; ++i) {
 			std::random_device rd;
 			std::mt19937 gen(rd());
-			std::uniform_int_distribution<> dist(min, max);//создаем рандомное число в промежутке от мин до макс
+			std::uniform_int_distribution<> dist(min, max);
 			_data[i] = new Pair{ i, dist(gen), true };
+		}
+	}
+
+	void grow() {
+		std::vector<Pair<Key, Value>> tmp;
+		tmp.resize(_size * 1.7);
+		std::swap(tmp, _data);
+		_size = 0;
+		for (auto i = 0; i < tmp.size(); ++i) {
+			if (tmp[i].is_filled) {
+				insert(tmp[i].key, tmp[i].value);
+			}
+		}
+	}
+
+	void insert(const Key& key, const Value& value) {
+		if (_size) {
+			double load_factor = _count_pairs / (_size + 0.0);
+			if (load_factor > 0.6) grow();
+		}
+		for (size_t i = 0; i < _data.size(); ++i)
+		{
+			size_t index = (hash(key) + i * std::hash<Key>{}(key)) % _data.size();
+			if (!_data[index].is_filled)
+			{
+				_data[index] = Pair{ key, value, true };
+				++_count_pairs;
+				return;
+			}
+			if (_data[index].key == key) throw std::logic_error("key is exist");
+		}
+	}
+
+	void insert_or_assign(const Key& key, const Value& value) {
+		if (_size) {
+			double load_factor = _count_pairs / (_size + 0.0);
+			if (load_factor > 0.6) grow();
+		}
+		for (size_t i = 0; i < _data.size(); ++i) {
+			size_t index = (hash(key) + i * std::hash<Key>{}(key)) % _data.size();
+			if (!_data[index].is_filled) {
+				_data[index] = Pair{ key, value, true };
+				++_count_pairs;
+				return;
+			}
+			if (_data[index].key == key) {
+				_data[index].value = value;
+				return;
+			}
 		}
 	}
 
 	size_t get_size() {
 		return _size; 
+	}
+
+	size_t get_count() {
+		return _count_pairs;
 	}
 
 	size_t hash(const Key& key) {
--- tests/hash_tests.cc
+++ tests/hash_tests.cc
@@ -11,4 +11,22 @@ TEST(pair_test, constructor_params) {
 TEST(hash_table_test, default_constr) {
 	HashTable<int,int> ht(11);
 	ASSERT_EQ(ht.get_size(),11);
+	ASSERT_EQ(ht.get_count(), 0);
+}
+
+TEST(hash_table_test, insert) {
+	HashTable<int, int> ht(11);
+	ASSERT_EQ(ht.get_count(), 0);
+	ht.insert(12, 4);
+	ASSERT_EQ(ht.get_count(), 1);
+	ASSERT_THROW(ht.insert(12, 5), std::logic_error);
+}
+
+TEST(hash_table_test, insert_or_assign) {
+	HashTable<int, int> ht(11);
+	ASSERT_EQ(ht.get_count(), 0);
+	ht.insert(12, 4);
+	ASSERT_EQ(ht.get_count(), 1);
+	ht.insert_or_assign(12, 5);
+	ASSERT_EQ(ht.get_count(), 1);
 }
